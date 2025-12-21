9d5557e4a6d227e4d6bae7c22d018b8701fb7284
kamilshan <kamiljavaid24@gmail.com>
2025-12-21 23:07:17 +0530

    added all the files

--- fahrenheittocelsius.cpp
+++ fahrenheittocelsius.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 int main()
 {
-	int ftemp;
-	cout<<"enter temperature in fahrenheit";
-	cin>>ftemp;
-	int ctemp=(ftemp-32)*5/9;
-	cout <<"equilavent in celsius is:" <<ctemp<< '\n';
-	return 0;
+    int ftemp;
+    cout<<"enter temperature in fahrenheit";
+    cin>>ftemp;
+    int ctemp=(ftemp-32)*5/9;
+    cout <<"equivalent in celsius is:" <<ctemp<< '\n';
+    return 0;
 }
