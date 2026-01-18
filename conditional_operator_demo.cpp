36f5c824b8ff656b7237da92b74b4b1d421eec15
kamilshan <kamiljavaid24@gmail.com>
2026-01-18 19:19:30 +0530

    added conditional operator demo

---
+++ conditional_operator_demo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int tu,alpha,beta;
+    cout<<"enter tu";
+    cin>>tu;
+    
+    cout<<"enter alpha";
+    cin>>alpha;
+    cout<<"enter beta";
+    cin>>beta;
+    tu=(alpha<beta) ? alpha : beta;
+    cout<<alpha<<"\n"<<beta<<"\n"<<tu;
+    
+
+    return 0;
+}
